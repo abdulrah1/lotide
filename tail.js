c335934bece21b405b271db004ac5265492346fd
Abdul <ffred7760@gmail.com>
2022-11-15 10:19:51 -0500

    added the tail function with multiple test cases

--- tail.js
+++ tail.js
@@ -6,11 +6,16 @@ let assertEqual = function(actual, expected) {
   console.log(`❌❌❌ Assertation Failed: ${actual} !== ${expected}`); // Do NOT Need an Else statement
 
 };
-
+// the actual === expected is NOT going to work because???
+//
 function tail (array) {
-  if (array.length > 1) {
+  if (array.length > 1) { //
     return array.slice(1);
   }
 }
 
-assertEqual(tail([1,2,3,4]), ([2,3,4]))
+const result = tail(["Hello", "Lighthouse", "Labs"]); // First we provided an array for the tails
+assertEqual(result.length, 2); // Tested to see if the actual vs expected is TRUE (yes)
+assertEqual(result[0], "Lighthouse"); // Tested to see if the first element of the tail array is "Lighthouse" (yes)
+assertEqual(result[1], "Labs"); 
+assertEqual(result[0], "Labs") // This is FALSE thus, it is !== 
