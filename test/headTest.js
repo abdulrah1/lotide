5ddc61f70db23532a1f58a7b15f5152e2fe0642a
Abdul <ffred7760@gmail.com>
2022-11-21 15:01:51 -0500

    Converted headTest.js to use Mocha and Chai to test the function; Works

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,43 @@
+// const head = require('../head'); // commented out since we are converting to use Mocha and Chai and it includes the code.
+const assertEqual = require('../assertEqual');
+
+
+// converting headTest.js to use Mocha and Chai
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require ('../assertEqual');
 
-///// Below are the Test Codes:
+describe("#head", () => {
+  it("should returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("should return '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); // If put 5 instead of '5' >>> failed because '5' is not equal to 5 since we are doing "strictEqual"
+  });
+  it('should return 5 for [5, 6, 7]', () => {
+    assert.strictEqual(head([5, 6, 7]), 5); // Pass
+  });
+  it("should return 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Pass 
+  });
+});
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///// Below are the Test Codes (We No Longer Need These; Keeping for reference only)
+/*
 assertEqual(head([5, 6, 7]), 5); // The second argument is what "expected"
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5]), 5);
+*/
