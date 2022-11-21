8f669fb61f1f9d5a7d29a37c8bf74bad07962d18
Abdul <ffred7760@gmail.com>
2022-11-21 13:59:03 -0500

    added test/eqArraysTest.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,33 +1,3 @@
-let assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔✔✔ Assertation Passed: ${actual} === ${expected}`);
-    return; // This stops executing the function and jumps into the next line of code.
-  }
-  console.log(`❌❌❌ Assertation Failed: ${actual} !== ${expected}`); // Do NOT Need an Else statement
-
-};
-
-
-// Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match
-
-/*
-
-var eqArrays = function(arr1, arr2) {
-
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-*/
-
-
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     // console.log("Arrays not equal length")
@@ -42,10 +12,12 @@ const eqArrays = function(array1, array2) {
       // if the element is not an array and the items are not equal then return false
       return false;
     }
-  } 
+  }
   return true;
 };
 
+
+module.exports = eqArrays;
 
 
 
@@ -63,4 +35,4 @@ const eqArrays = function(array1, array2) {
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 
 // eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+//assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
